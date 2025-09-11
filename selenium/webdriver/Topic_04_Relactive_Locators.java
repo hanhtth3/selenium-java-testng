64a369795da429e01a33eab1baa9ebc053954654
HanhTTH <hanhtth3@gmail.com>
2025-09-11 21:01:20 +0700

    Update types of locator

--- selenium/webdriver/Topic_04_Relactive_Locators.java
+++ selenium/webdriver/Topic_04_Relactive_Locators.java
@@ -4,24 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chromium.ChromiumDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.locators.RelativeLocator;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_04_Relactive_Locator {
+public class Topic_04_Relactive_Locators {
     WebDriver driver;
 
     @BeforeClass
     public void beforeClass() {
        // driver = new FirefoxDriver();
         driver = new ChromeDriver();
-        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
+
     }
     @Test
     public void TC_01() {
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2F");
         WebElement rememberMeText = driver.findElement(RelativeLocator.with(By.tagName("label"))
                 //Bên trên button Login
                 .above(By.cssSelector("button.login-button"))
@@ -33,8 +32,20 @@ public class Topic_04_Relactive_Locator {
                 .toLeftOf(By.xpath("//a[text()='Forgot password?']")));
 
         System.out.println(rememberMeText.getText());
+    }
 
-
+    @Test
+    public void TC_02_() {
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+        driver.findElement(By.xpath("//input[@id='gender-male']"));
+        driver.findElement(By.xpath ("//input[@value='M']"));
+        driver.findElement(By.xpath ("//input[@id='FirstName']"));
+        driver.findElement(By.xpath ("//input[@name='LastName']"));
+        driver.findElement(By.xpath ("//input[@name='Email']"));
+        driver.findElement(By.xpath ("//input[@id='Company']"));
+        driver.findElement(By.xpath ("//input[@id='Newsletter']"));
+        driver.findElement(By.xpath ("//input[@id='Password']"));
+        driver.findElement(By.xpath ("//input[@id='ConfirmPassword']"));
     }
 
     @AfterClass
